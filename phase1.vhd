f24b92f16e038d9f2681743b9aab2d720cd42f68
WholeCream <38162078+eprosync@users.noreply.github.com>
2023-01-30 16:00:52 -0500

    init phase 1 file

---
+++ phase1.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.system_components.all;
+
+entity phase1 is
+	port (
+	)
+end entity;
+
+architecture arch_phase1 of phase1 is
+
+begin
+
+end architecture;

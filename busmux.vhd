ad0e259fd6e1faad32c333facf476dd1929cb09a
WholeCream <38162078+eprosync@users.noreply.github.com>
2023-02-12 08:37:36 -0500

    phase1 block diagram

--- busmux.vhd
+++ busmux.vhd
@@ -3,30 +3,30 @@ use ieee.std_logic_1164.all;
 
 entity BusMux is
 	port (
-		R0,
-		R1,
-		R2,
-		R3,
-		R4,
-		R5,
-		R6,
-		R7,
-		R8,
-		R9,
-		R10,
-		R11,
-		R12,
-		R13,
-		R14,
-		R15,
-		HI,
-		LO,
-		ZHI,
-		ZLO,
-		PC,
-		MDR,
-		INPORT,
-		C
+		R0_BMin,
+		R1_BMin,
+		R2_BMin,
+		R3_BMin,
+		R4_BMin,
+		R5_BMin,
+		R6_BMin,
+		R7_BMin,
+		R8_BMin,
+		R9_BMin,
+		R10_BMin,
+		R11_BMin,
+		R12_BMin,
+		R13_BMin,
+		R14_BMin,
+		R15_BMin,
+		HI_BMin,
+		LO_BMin,
+		ZHI_BMin,
+		ZLO_BMin,
+		PC_BMin,
+		MDR_BMin,
+		INPORT_BMin,
+		C_BMin
 		: in std_logic_vector (31 downto 0);
 	
 		S : in std_logic_vector (4 downto 0);
@@ -37,28 +37,28 @@ end entity;
 architecture behavior of BusMux is
 	
 begin
-	BusMuxOut <= R0 when (S="00000") else
-		R1 when (S="00001") else
-		R2 when (S="00010") else
-		R3 when (S="00011") else
-		R4 when (S="00100") else
-		R5 when (S="00101") else
-		R6 when (S="00110") else
-		R7 when (S="00111") else
-		R8 when (S="01000") else
-		R9 when (S="01001") else
-		R10 when (S="01010") else
-		R11 when (S="01011") else
-		R12 when (S="01100") else
-		R13 when (S="01110") else
-		R14 when (S="01111") else
-		R15 when (S="10000") else
-		HI when (S="10001") else
-		LO when (S="10010") else
-		ZHI when (S="10011") else
-		ZLO when (S="10100") else
-		PC when (S="10101") else
-		INPORT when (S="10110") else
-		C when (S="10111") else
+	BusMuxOut <= R0_BMin when (S="00000") else
+		R1_BMin when (S="00001") else
+		R2_BMin when (S="00010") else
+		R3_BMin when (S="00011") else
+		R4_BMin when (S="00100") else
+		R5_BMin when (S="00101") else
+		R6_BMin when (S="00110") else
+		R7_BMin when (S="00111") else
+		R8_BMin when (S="01000") else
+		R9_BMin when (S="01001") else
+		R10_BMin when (S="01010") else
+		R11_BMin when (S="01011") else
+		R12_BMin when (S="01100") else
+		R13_BMin when (S="01110") else
+		R14_BMin when (S="01111") else
+		R15_BMin when (S="10000") else
+		HI_BMin when (S="10001") else
+		LO_BMin when (S="10010") else
+		ZHI_BMin when (S="10011") else
+		ZLO_BMin when (S="10100") else
+		PC_BMin when (S="10101") else
+		INPORT_BMin when (S="10110") else
+		C_BMin when (S="10111") else
 		"00000000000000000000000000000000";
 end architecture;
